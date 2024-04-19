616c159ba9d41d30e0f6aa794f55a76470b7789f
Igor Gomes <igor23gomes2@gmail.com>
2024-04-19 19:25:38 -0300

    migration liking company to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_19_110410) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_19_222104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,9 +64,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_19_110410) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
   end
 
   add_foreign_key "product_orders", "products"
   add_foreign_key "product_orders", "users"
   add_foreign_key "products", "suppliers"
+  add_foreign_key "users", "companies"
 end
